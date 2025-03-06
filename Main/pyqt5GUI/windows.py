5ec465a3bcbd2c74f6cade8df6aca96f8665a5d3
starek8 <piotr.buchman@ssp26.pl>
2025-03-06 09:50:47 +0100

    działa ale bez refaktoryzacji

--- Main/pyqt5GUI/windows.py
+++ Main/pyqt5GUI/windows.py
@@ -1,8 +1,6 @@
-from pickle import FALSE
+from xml.etree.ElementTree import tostring
 
 from PyQt5.QtWidgets import QPushButton, QWidget, QLabel, QFileDialog, QCheckBox
-
-
 
 class App(QWidget):
     def __init__(self):
@@ -12,97 +10,91 @@ class App(QWidget):
         self.top = 100
         self.width = 1000
         self.height = 1000
-        self.initUI()
-        self.file = False
+        self.main_window()
         self.directory = False
+        self.selected_input_path = " "
+        self.selected_output_path = " "
 
-    def initUI(self):
+    def main_window(self):
+        #First thing first
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.button_select_input_filepath = QPushButton("Select Input File Path", self)
-        self.button_select_input_filepath.move(100, 70)
-        self.button_select_input_filepath.clicked.connect(self.on_button_select_input_filepath_click)
-        self.button_select_input_filepath.hide()
+        #select in_file button
+        self.button_select_input_path = QPushButton("Select Input File Path", self)
+        self.button_select_input_path.move(100, 70)
+        self.button_select_input_path.clicked.connect(self.on_button_select_input_path_click)
 
-        self.label_input_filepath = QLabel("Input File Path", self)
-        self.label_input_filepath.adjustSize()
-        self.label_input_filepath.move(120, 120)
+        #label for select in_file path
+        self.label_input_path = QLabel("Input File Path", self)
+        self.label_input_path.adjustSize()
+        self.label_input_path.move(120, 120)
 
-        self.button_select_output_filepath = QPushButton("Select Output File Path", self)
-        self.button_select_output_filepath.move(100, 150)
-        self.button_select_output_filepath.clicked.connect(self.on_button_select_output_filepath_click)
-        self.button_select_output_filepath.hide()
+        #select out_file button
+        self.button_select_output_path = QPushButton("Select Output File Path", self)
+        self.button_select_output_path.move(100, 150)
+        self.button_select_output_path.clicked.connect(self.on_button_select_output_filepath_click)
 
-        self.label_output_filepath = QLabel("Output File Path", self)
-        self.label_output_filepath.adjustSize()
-        self.label_output_filepath.move(120, 200)
+        #lebel for selected out_file path
+        self.label_output_path = QLabel("Output File Path", self)
+        self.label_output_path.adjustSize()
+        self.label_output_path.move(120, 200)
 
-        self.checkbox_file = QCheckBox("File", self)
-        self.checkbox_file.setGeometry(50, 250, 100, 30)
-        self.checkbox_file.stateChanged.connect(self.on_checkbox_click_file)
+        #button for future use
+        self.button_future_use = QPushButton("Future use button",self)
+        self.button_future_use.move(100, 400)
+        self.button_future_use.clicked.connect(self.on_button_future_use_function)
 
+        #check box for if files are in or out
         self.checkbox_directory = QCheckBox("dir", self)
-        self.checkbox_directory.setGeometry(50,300,100,30)
+        self.checkbox_directory.setGeometry(50, 250, 100, 50)
         self.checkbox_directory.stateChanged.connect(self.on_checkbox_click_directory)
 
+        #shows all
         self.show()
 
-    #dialogi:
-    def on_button_select_input_filepath_click(self):
-        selected_file_path = ""
-        if self.file:
-            file_name, _ = QFileDialog.getOpenFileNames(self, "Select File", "", "All Files (*)")
+    #dialog for input button
+    def on_button_select_input_path_click(self):
+        if self.directory:
+            file_name = QFileDialog.getExistingDirectory(self, "Select Directory")
         else:
-            file_name= QFileDialog.getExistingDirectory(self, "Select Directory")
+            file_name, _ = QFileDialog.getOpenFileNames(self, "Select File", "", "All Files (*)")
 
         if file_name:
-            selected_file_path = file_name[0] if isinstance(file_name, list) else file_name
-            self.label_input_filepath.setText(f"Selected file: {selected_file_path}")
-            self.label_input_filepath.adjustSize()
-            print(file_name)
+            self.selected_input_path = file_name[0] if isinstance(file_name, list) else file_name
+            self.label_input_path.setText(f"Selected file: {self.selected_input_path}")
+            self.label_input_path.adjustSize()
         else:
-            self.label_input_filepath.setText("No file selected")
-            self.label_input_filepath.adjustSize()
-        return selected_file_path
+            self.label_input_path.setText("No file selected")
+            self.label_input_path.adjustSize()
 
+    #dialog for output button
     def on_button_select_output_filepath_click(self):
-        selected_file_path = ""
-        if self.file:
-            file_name, _ = QFileDialog.getOpenFileNames(self, "Select File", "", "All Files (*)")
-        else:
+        if self.directory:
             file_name= QFileDialog.getExistingDirectory(self, "Select Directory")
+        else:
+            file_name, _ = QFileDialog.getOpenFileNames(self, "Select File", "", "All Files (*)")
 
         if file_name:
-            selected_file_path = file_name[0] if isinstance(file_name, list) else file_name
-            self.label_output_filepath.setText(f"Selected file: {selected_file_path}")
-            self.label_output_filepath.adjustSize()
+            self.selected_output_path = file_name[0] if isinstance(file_name, list) else file_name
+            self.label_output_path.setText(f"Selected file: {self.selected_output_path}")
+            self.label_output_path.adjustSize()
         else:
-            self.label_output_filepath.setText("No file selected")
-            self.label_output_filepath.adjustSize()
+            self.label_output_path.setText("No file selected")
+            self.label_output_path.adjustSize()
 
-        return selected_file_path
+    #button for future use space for function
+    def on_button_future_use_function(self):
+        #put future function here
+        # function_name(self.selected_input_path, self.selected_output_path , self.directory)
+        ################        string                     string               bool
+        #and then comment return
+        return
+        #print(self.selected_input_path + "    " + self.selected_output_path + "     " + f"{self.directory}")
 
-    def on_checkbox_click_file(self, state):
-        if state:
-            self.checkbox_directory.hide()
-            self.button_select_input_filepath.show()
-            self.button_select_output_filepath.show()
-            self.file = True
-        else:
-            self.checkbox_directory.show()
-            self.button_select_input_filepath.hide()
-            self.button_select_output_filepath.hide()
-            self.file = False
-
+    #checkbox if directory
     def on_checkbox_click_directory(self, state):
         if state:
-            self.checkbox_file.hide()
-            self.button_select_input_filepath.show()
-            self.button_select_output_filepath.show()
             self.directory = True
         else:
-            self.checkbox_file.show()
-            self.button_select_input_filepath.hide()
-            self.button_select_output_filepath.hide()
             self.directory = False
