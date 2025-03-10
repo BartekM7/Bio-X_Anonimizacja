529459dc06f7522753264942af0eb4f61201d75b
Oliwia <123oliwka.wozniak123@gmail.com>
2025-03-10 14:16:36 +0100

    GUI (chyba lepiej)

--- Main/pyqt5GUI/windows.py
+++ Main/pyqt5GUI/windows.py
@@ -1,52 +1,55 @@
 from PyQt5.QtWidgets import QPushButton, QWidget, QLabel, QFileDialog, QCheckBox
+from PyQt5.QtGui import QIcon
 
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = "apka"
-        self.left = 100
-        self.top = 100
-        self.width = 1000
-        self.height = 1000
+        self.title = "Anonimizacja plików DCM"
+        self.setWindowIcon(QIcon("/Main/pyqt5GUI/Assets/ikonka_testowa.png"))
+        self.left = 50
+        self.top = 50
+        self.width = 500
+        self.height = 200
         self.main_window()
         self.directory = False
         self.selected_input_path = " "
         self.selected_output_path = " "
+
 
     def main_window(self):
         #First thing first
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        #select in_file button
-        self.button_select_input_path = QPushButton("Select Input File Path", self)
-        self.button_select_input_path.move(100, 70)
-        self.button_select_input_path.clicked.connect(self.on_button_select_input_path_click)
-
-        #label for select in_file path
+        # label for select in_file path
         self.label_input_path = QLabel("Input File Path", self)
         self.label_input_path.adjustSize()
-        self.label_input_path.move(120, 120)
+        self.label_input_path.move(20, 10)
+
+        #select in_file button
+        self.button_select_input_path = QPushButton("Select Input File Path", self)
+        self.button_select_input_path.move(10, 30)
+        self.button_select_input_path.clicked.connect(self.on_button_select_input_path_click)
+
+        # lebel for selected out_file path
+        self.label_output_path = QLabel("Output File Path", self)
+        self.label_output_path.adjustSize()
+        self.label_output_path.move(20, 70)
 
         #select out_file button
         self.button_select_output_path = QPushButton("Select Output File Path", self)
-        self.button_select_output_path.move(100, 150)
+        self.button_select_output_path.move(10, 90)
         self.button_select_output_path.clicked.connect(self.on_button_select_output_filepath_click)
-
-        #lebel for selected out_file path
-        self.label_output_path = QLabel("Output File Path", self)
-        self.label_output_path.adjustSize()
-        self.label_output_path.move(120, 200)
-
-        #button for future use
-        self.button_future_use = QPushButton("Future use button",self)
-        self.button_future_use.move(100, 400)
-        self.button_future_use.clicked.connect(self.on_button_future_use_function)
 
         #check box for if files are in or out
         self.checkbox_directory = QCheckBox("dir", self)
-        self.checkbox_directory.setGeometry(50, 250, 100, 50)
+        self.checkbox_directory.setGeometry(20, 110, 100, 50)
         self.checkbox_directory.stateChanged.connect(self.on_checkbox_click_directory)
+
+        # button for future use
+        self.button_future_use = QPushButton("Start", self)
+        self.button_future_use.move(10, 160)
+        self.button_future_use.clicked.connect(self.on_button_future_use_function)
 
         #shows all
         self.show()
@@ -96,3 +99,19 @@ class App(QWidget):
             self.directory = True
         else:
             self.directory = False
+
+# class SuccesWindow(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         super().title = "Success"
+#         self.width = 200
+#         self.height = 100
+#
+#     def succes_window(self):
+#         self.setWindowTitle(self.title)
+#         self.setGeometry(self.left, self.top, self.width, self.height)
+#
+#         # label for select in_file path
+#         self.label_input_path = QLabel("Success", self)
+#         self.label_input_path.adjustSize()
+#         self.label_input_path.move(20, 10)
